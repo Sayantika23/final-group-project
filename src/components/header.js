e07ff77520ce868e9c915d6324b6d26ba39fc181
Jasmine <ly4938@rit.edu>
2019-03-19 22:36:21 -0400

    03192919

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,76 @@
 import React from "react";
 import "./header.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <img className="Logo" src={require("../photos/logo.png")} />
+        <Link to="/" onClick="window.location.reload()">
+          <img className="Logo" src={require("../photos/logo.png")} />
+        </Link>
         <div className="Main-Nav">
-          <div className="Menu"><h1>NEWS</h1></div>
           <div className="Menu">
-            <h1>TOURNAMENTS</h1>
-            <img src={require("../photos/chevron-down.png")} />
+            <a href="https://www.ittf.com/news/">
+              <h1>NEWS</h1>
+            </a>
           </div>
           <div className="Menu">
-            <h1>PLAYERS</h1>
-            <img src={require("../photos/chevron-down.png")} />
+            <div className="Menu1">
+              <h1>TOURNAMENTS</h1>
+              <img src={require("../photos/chevron-down.png")} />
+            </div>
+            <div className="DropDown">
+              <a href="#">World Championships</a>
+              <a href="#">Upcoming</a>
+              <a href="https://www.ittf.com/tournaments/">Calender</a>
+            </div>
           </div>
           <div className="Menu">
-            <h1>WATCH</h1>
-            <img src={require("../photos/chevron-down.png")} />
+            <div className="Menu1">
+              <h1>PLAYERS</h1>
+              <img src={require("../photos/chevron-down.png")} />
+            </div>
+            <div className="DropDown">
+              <a href="https://www.ittf.com/rankings/">Ranking</a>
+              <a href="http://results.ittf.link/">Stat</a>
+            </div>
           </div>
-          <div className="Menu"><h1>TICKETS</h1></div>
-          <div className="Menu"><h1>STORE</h1></div>
           <div className="Menu">
-            <h1>MORE</h1>
-            <img src={require("../photos/chevron-down.png")} />
+            <div className="Menu1">
+              <h1>WATCH</h1>
+              <img src={require("../photos/chevron-down.png")} />
+            </div>
+            <div className="DropDown">
+              <a href="https://tv.ittf.com/">ITTV</a>
+              <a href="#">Broadcast</a>
+            </div>
+          </div>
+          <div className="Menu">
+            <a href="https://wttc2019.jegy.hu/programseries/liebherr-2019-ittf-asztalitenisz-vilagbajnoksag-11/">
+              <h1>TICKETS</h1>
+            </a>
+          </div>
+          <div className="Menu">
+            <h1>STORE</h1>
+          </div>
+          <div className="Menu">
+            <div className="Menu1">
+              <h1>MORE</h1>
+              <img src={require("../photos/chevron-down.png")} />
+            </div>
           </div>
         </div>
         <div className="Side-Nav">
-          <div className="Menu">中文</div>
+          <a href="https://cn.ittf.com/">
+            <div className="Menu">
+              <h1>中文</h1>
+            </div>
+          </a>
           <img src={require("../photos/search.svg.png")} />
-          <div className="Menu">SIGN UP/LOG IN</div>
+          <div className="Menu">
+            <h1>SIGN UP/LOG IN</h1>
+          </div>
         </div>
       </div>
     );
--- src/components/score.js
+++ src/components/score.js
@@ -2,23 +2,44 @@ import React from "react";
 import "./score.css";
 
 export default class Score extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+    };
+    this.plus = this.plus.bind(this);
+  }
+
+  plus() {
+    this.setState({
+      index: this.state.index + 1
+    });
+  }
+
   render() {
+    let slideshowContainer = {
+      transform: ""
+    };
+    let trans = -300 * this.state.index;
+    if (this.state.index < 5) {
+      slideshowContainer.transform = "translateX(" + trans + "px)";
+    }
     return (
       <div className="Score">
         <a className="Left">
           <img src={require("../photos/chevron-left.png")} />
         </a>
-        <a className="Right">
+        <a className="Right" onClick={this.plus}>
           <img src={require("../photos/chevron-right.png")} />
         </a>
         <div className="Score1">
-          <div className="Score-Container">
-            <img src={require("../photos/score cell.png")} />
-            <img src={require("../photos/score cell.png")} />
-            <img src={require("../photos/score cell.png")} />
-            <img src={require("../photos/score cell.png")} />
-            <img src={require("../photos/score cell.png")} />
-            <img src={require("../photos/score cell.png")} />
+          <div className="Score-Container"  style={slideshowContainer}>
+            <img className="fade" src={require("../photos/score cell.png")} />
+            <img className="fade" src={require("../photos/score cell.png")} />
+            <img className="fade" src={require("../photos/score cell.png")} />
+            <img className="fade" src={require("../photos/score cell.png")} />
+            <img className="fade" src={require("../photos/score cell.png")} />
+            <img className="fade" src={require("../photos/score cell.png")} />
           </div>
         </div>
       </div>
